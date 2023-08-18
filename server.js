ed1a4c0077f59dbe7435cf1babec00144461b0cb
Minstreln <minstreln@gmail.com>
2023-08-19 00:52:23 +0100

    Update server.js

--- server.js
+++ server.js
@@ -27,8 +27,9 @@ mongoose
 
 const app = require('./app');
 
+const host = "0.0.0.0";
 const port = process.env.PORT || 5000;
-const server = app.listen(port, () => {
+const server = app.listen(port, host, () => {
   console.log(`App running on port ${port}...`);
 });
 
