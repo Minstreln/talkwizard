cfc9b8b8bb07deb0042f4058fd43e7266aa88017
Minstreln <minstreln@gmail.com>
2023-08-18 19:48:37 +0100

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const mongoose = require('mongoose');
 
 const dotenv = require('dotenv');
 
+const path = require('path');
+
 process.on('uncaughtException', (err) => {
   console.log(err.name, err.message);
   process.exit(1);
